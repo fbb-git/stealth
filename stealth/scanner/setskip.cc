d71fa134bf2128a971539be3e0d27943b98c5c82
Frank B. Brokken <f.b.brokken@rug.nl>
2011-06-23 08:14:38 +0000

    Ready for 2.02.01
    git-svn-id: https://stealth.svn.sourceforge.net/svnroot/stealth/trunk@191 93b9aca8-391c-0410-bfd7-9a5413174b8b

--- stealth/scanner/setskip.cc
+++ stealth/scanner/setskip.cc
@@ -5,9 +5,14 @@ void Scanner::setSkip(string const &fname)
     ifstream in;
     Errno::open(in, fname);
 
-    for_each(istream_iterator<Line>(in), istream_iterator<Line>(), 
-            FnWrap::unary(add, d_skipFiles));
-
+    for_each(
+        istream_iterator<Line>(in), istream_iterator<Line>(), 
+        [&](std::string const &line)
+        {
+            add(line, d_skipFiles);
+        }
+    );
+    
     d_skip = &Scanner::skip;
 }
 
--- stealth/scanner/skip.cc
+++ stealth/scanner/skip.cc
@@ -1,19 +1,17 @@
 #include "scanner.ih"
 
-namespace {
-
-    bool skipTarget(string targetLine, string outputLine)
-    {
-        size_t idx = outputLine.rfind(targetLine);
-        return idx + targetLine.length() == outputLine.length();
-    }
-}
-
 bool Scanner::skip(string const &line)
 {
     auto end = d_skipFiles.end();
-    return find_if(d_skipFiles.begin(), end, 
-                   FnWrap::unary(skipTarget, line)) != end;
+    return find_if(
+                d_skipFiles.begin(), end, 
+                [&](string const &targetLine)
+                {
+                    size_t idx = line.rfind(targetLine);
+                    return idx + targetLine.length() == line.length();
+                }
+            )
+            != end;
 }
 
 
