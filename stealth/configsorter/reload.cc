773c5adb0b33f6bea8ac07c954116b14300cb168
Frank B. Brokken <f.b.brokken@rug.nl>
2012-06-09 13:15:13 +0200

    WIP on flow control for reload: added configsorter/reload

---
+++ stealth/configsorter/reload.cc
@@ -0,0 +1,28 @@
+#include "configsorter.ih"
+
+void ConfigSorter::reload(string const &confFile)
+{
+    d_configfile.open(confFile);
+
+    d_use = HashString<std::string>
+                (&s_defaultKeyword[0], &s_defaultKeyword[s_nDefaultKeywords]);
+
+    d_define.clear();
+    d_command.clear();
+
+    fetchCommands();
+
+    string &base = d_use["BASE"];
+
+    base += "/.";                           // the . is required by mkdir
+
+    char const *cp = base.c_str();
+
+    if (!Util::mkdir(cp) || chdir(cp))
+        fmsg << "Can't chdir to `" << cp << '\'' << endl;
+
+    base.resize(base.length() - 1);         // cut off the . again
+}
+
+
+
